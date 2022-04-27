dabbebe97bc389b2de5e3da7358845367de77ba9
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-04-27 18:57:10 +0430

    detect which cards are matched

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/singleCard/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 function App() {
   const [cards, setCards] = useState([]);
@@ -23,29 +23,26 @@ function App() {
     setTurn(0);
   }
 
-  // compare the selected cards
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      if (choiceOne.src === choiceTwo.src) {
-        console.log('yesss')
-        resetTurn();
-      }
-      else {
-        console.log('noooo')
-        resetTurn();
-      }
-    }
-  }, [choiceOne, choiceTwo])
-
-
-  const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  }
-
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurn(turn + 1);
+  }
+  // compare the selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(cards.map(card => card.src === choiceOne.src ? { ...card, matched: true } : card));
+        resetTurn();
+      }
+      else {
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo,resetTurn])
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   }
 
   return (
