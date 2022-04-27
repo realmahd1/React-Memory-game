7a85a46b2e131b608deb08189255ae2796f639e0
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-04-27 18:02:39 +0430

    add shuffle cards function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './App.css'
 
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" },
+]
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turn, setTurn] = useState(0);
+  //shuffle the cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurn(0);
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
